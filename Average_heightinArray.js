341e55ddb108f98b6fcf7603f54724776934aa2b
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2022-11-07 16:45:02 +0530

    Average In Array Using JS

---
+++ Average_heightinArray.js
@@ -0,0 +1,16 @@
+//Given the heights of n students of a class, find the average height. Your answer should be accurate upto 5 decimal places.
+
+function(n,arr){
+    let sum = 0;
+    for(let i = 0; i< arr.length; i++){
+        sum = sum + arr[i]
+    }
+    return parseFloat(sum/n)
+}
+
+function main() {
+    let n=parseInt(readLine(),10);
+    let arr=readFloatArr();
+    let result=averageHeight(n,arr);
+    print(result);
+}
