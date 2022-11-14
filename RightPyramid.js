85d24ef717d941cee808a8eb8fc3de09de6852d0
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2022-11-14 21:18:29 +0530

    Pyramid Implementation using JS

---
+++ RightPyramid.js
@@ -0,0 +1,9 @@
+let pyramidLeft = (rows) => {
+    for(let i = 1; i <= rows; i++){
+      let str = '';
+      for(let j = 1; j <= i; j++){
+        str += '*';
+      }
+      console.log(str);
+    }
+ };
